d28df8d5af0df8602409fa9c9c52d66d6b55f4a2
zhumingliang <353575156@qq.com>
2020-07-21 23:55:30 +0800

    增加图片服务

---
+++ app/business/ImageBusiness.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace app\business;
+
+
+use app\lib\exception\SaveException;
+
+class ImageBusiness
+{
+    public function upload($file)
+    {
+        $path = dirname($_SERVER['SCRIPT_FILENAME']) . '/static/image';
+        if (!is_dir($path)) {
+            mkdir(iconv("UTF-8", "GBK", $path), 0777, true);
+        }
+        $info = $file->move($path);
+        if (!$info) {
+            throw new SaveException();
+        }
+        return ['url' => '/static/image/' . $info->getSaveName()];
+    }
+
+    public function saveCompanyQRCode($url)
+    {
+        $path = dirname($_SERVER['SCRIPT_FILENAME']) . '/static/qrcode';
+        if (!is_dir($path)) {
+            mkdir(iconv("UTF-8", "GBK", $path), 0777, true);
+        }
+        $content = file_get_contents($url); // 得到二进制图片内容
+        $name = guid();
+        $info = file_put_contents($path . "/$name.jpg", $content); // 写入文件
+        if (!$info) {
+            throw new SaveException();
+        }
+        return '/static/qrcode' . "/$name.jpg";
+    }
+
+}
