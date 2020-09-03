d05d7bb539c10db90ffe837d22b3085e21a30d39
zhumingliang <353575156@qq.com>
2020-09-04 00:09:53 +0800

    优化凌晨扣费

--- app/business/OrderBusiness.php
+++ app/business/OrderBusiness.php
@@ -5,6 +5,8 @@ namespace app\business;
 
 
 use app\lib\enum\CommonEnum;
+use app\model\OrderParentT;
+use app\model\OrderSubT;
 use app\model\OrderT;
 use app\model\OrderUnusedV;
 
@@ -13,19 +15,46 @@ class OrderBusiness
     public function handelUnusedOrder()
     {
         $consumption_time = date('Y-m-d');
-        $dataList = [];
+        $oneDataList = [];
+        $moreDataList = [];
+        $parentDataList = [];
         $orders = (new  OrderUnusedV())->orders($consumption_time);
         if (count($orders)) {
             foreach ($orders as $k => $v) {
-                array_push($dataList, [
-                    'id' => $v['id'],
-                    'consumption_type' => 'no_meals_ordered',
-                    'money' => 0,
-                    'unused_handel' => CommonEnum::STATE_IS_OK,
-                    'sub_money' => $v['no_meal_sub_money']
-                ]);
+                if ($v['strategy_type'] == 'one') {
+                    array_push($oneDataList, [
+                        'id' => $v['id'],
+                        'consumption_type' => 'no_meals_ordered',
+                        'money' => 0,
+                        'unused_handel' => CommonEnum::STATE_IS_OK,
+                        'sub_money' => $v['no_meal_sub_money']
+                    ]);
+                } else {
+                    array_push($moreDataList, [
+                        'id' => $v['id'],
+                        'consumption_type' => 'no_meals_ordered',
+                        'money' => 0,
+                        'unused_handel' => CommonEnum::STATE_IS_OK,
+                        'sub_money' => $v['no_meal_sub_money']
+                    ]);
+
+                    array_push($parentDataList, [
+                        'id' => $v['order_id'],
+                        'money' => $v['parent_money'] - $v['order_money'] - $v['order_sub_money'] + $v['no_meal_sub_money'],
+                    ]);
+
+                }
+
             }
-            (new OrderT())->saveAll($dataList);
+            if (count($oneDataList)) {
+                (new OrderT())->saveAll($oneDataList);
+            }
+            if (count($moreDataList)) {
+                (new OrderSubT())->saveAll($oneDataList);
+            }
+            if (count($parentDataList)) {
+                (new OrderParentT())->saveAll($oneDataList);
+            }
         }
     }
 
