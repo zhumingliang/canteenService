2918304da79c11f77e40ea289e774327e4a34b5d
zhumingliang <353575156@qq.com>
2020-12-04 22:58:53 +0800

    测试清零账户

---
+++ app/task/clearAccount.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace app\task;
+
+
+use app\model\LogT;
+use yunwuxin\cron\Task;
+
+class clearAccount extends Task
+{
+    public function configure()
+    {
+        $this->at("00:10:00");
+        //设置任务的周期，每天执行一次，更多的方法可以查看源代码，都有注释
+    }
+
+    /**
+     * 执行任务
+     * @return mixed
+     */
+    protected function execute()
+    {
+        LogT::create(['content' => '清除账户信息']);
+
+    }
+}
