fa097a79ad1e8ad0795d67ab38e175341e6825fe
zhumingliang <353575156@qq.com>
2020-12-17 17:33:18 +0800

    测试清零账户

--- app/task/clearAccount.php
+++ app/task/clearAccount.php
@@ -11,7 +11,7 @@ class clearAccount extends Task
 {
     public function configure()
     {
-        $this->at("23:10:00");
+        $this->dailyAt("20:00:01");
         //设置任务的周期，每天执行一次，更多的方法可以查看源代码，都有注释
     }
 
