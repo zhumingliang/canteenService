daf5b80899c5e13c14193e65e8c7c024114232e0
zhumingliang <353575156@qq.com>
2021-05-30 00:39:24 +0800

    惩罚策略更新

---
+++ app/task/Test.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace app\task;
+
+
+use app\business\OrderBusiness;
+use app\business\ReceptionBusiness;
+use app\model\TaskLogT;
+use yunwuxin\cron\Task;
+
+class Test extends Task
+{
+
+    public function configure()
+    {
+        $this->dailyAt("00:45");
+    }
+
+    /**
+     * 执行任务
+     * @return mixed
+     */
+    protected function execute()
+    {
+        try {
+            TaskLogT::create(['msg' => "开始测试："]);
+            (new OrderBusiness())->handelUnusedOrder();
+        } catch (\Exception $e) {
+            TaskLogT::create(['msg' => "批量处理未订餐就餐失败：" . $e->getMessage()]);
+        }
+
+    }
+
+}
